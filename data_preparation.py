f498d9433dfb5d58a48e5ffab9e3d7c73ce5f5c8
Sudheesh K S <sudheesh.ks.tech@gmail.com>
2025-09-04 23:37:44 +0530

    Data preprocesing completed

--- data_preparation.py
+++ data_preparation.py
@@ -1,38 +1,49 @@
 import pandas as pd
-
-print("loading the data")
-data = pd.read_csv('flood_risk_dataset_india.csv')
-
-print("Data Shape (Rows,Columns):", data.shape)
-print("\nFirst 5 rows:")
-print(data.head())
-print("\nColumn names:")
-print(data.columns)
-
-
-print("\nChecking for missing values...")
-print(data.isnull().sum())
-
-
-X = data.drop('Flood Occurred', axis=1) 
-
-y = data['Flood Occurred']
-
-print("\nFeatures shape (X):", X.shape)
-print("Target shape (y):", y.shape)
-
-
+import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-print(f"\nTraining Set: {X_train.shape}, {y_train.shape}")
-print(f"Testing Set: {X_test.shape}, {y_test.shape}")
+def main():
+    print("Data Preprocessing:")
+    
+    # Load the raw data
+    data = pd.read_csv('flood_risk_dataset_india.csv') 
+    print(f"   Loaded data shape: {data.shape}")
+
+    print("Checked for any missing values and there were no missing values.")
+
+    # Separate features and target
+    X = data.drop('Flood Occurred', axis=1)  
+    y = data['Flood Occurred']               
+   
+  
+
+  # Remove categorical columns before scaling
+    categorical_cols = X.select_dtypes(include=['object']).columns
+    X_numeric = X.drop(columns=categorical_cols)
+
+  # Split the data using X_numeric
+    X_train, X_test, y_train, y_test = train_test_split(
+    X_numeric, y, test_size=0.2, random_state=42, stratify=y)
+
+  # Scaling the numerical features 
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+  # Converting back to DataFrames
+    X_train_scaled = pd.DataFrame(X_train_scaled, columns=X_train.columns)
+    X_test_scaled = pd.DataFrame(X_test_scaled, columns=X_test.columns)
 
 
-X_train.to_csv('X_train.csv', index=False)
-X_test.to_csv('X_test.csv', index=False)
-y_train.to_csv('y_train.csv', index=False)
-y_test.to_csv('y_test.csv', index=False)
+    #  Save the processed data
+    X_train_scaled.to_csv('X_train_scaled.csv', index=False)
+    X_test_scaled.to_csv('X_test_scaled.csv', index=False)
+    y_train.to_csv('y_train.csv', index=False)
+    y_test.to_csv('y_test.csv', index=False)
+    print("   Saved scaled datasets to CSV.")
 
-print("\nCleaned and split data has been saved to CSV files!")
-print("\n completed!")
+    print("\n Preprocessing complete.")
+
+if __name__ == "__main__":
+    main()
